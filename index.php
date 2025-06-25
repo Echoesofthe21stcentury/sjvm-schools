327ca58eefc774662327ea1b08e1db750b781af6
Echoesofthe21stcentury <echoesofthe21stcentury@gmail.com>
2025-06-25 23:16:03 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>SJVM Schools</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Welcome to SJVM Schools</h1>
+  <h2>Where Education Meets Excellence</h2>
+  <a href="login.php">Login</a> | <a href="register.php">Admission / Register</a>
+</body>
+</html>
